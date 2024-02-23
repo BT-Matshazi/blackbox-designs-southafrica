5f32277e4277d17c060440f364095126ba5e0293
BT-Matshazi <bekithembamatshazi@gmail.com>
2024-02-23 19:06:44 +0200

    Update dependencies and add analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,29 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 import ToasterContext from "@/lib/context/ToasterContext";
-import { constructMetadata } from '@/lib/utils';
+import { constructMetadata } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = constructMetadata();
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         {children} <ToasterContext />
+        <Analytics />
       </body>
     </html>
   );

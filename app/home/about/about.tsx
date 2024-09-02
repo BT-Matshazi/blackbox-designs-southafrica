dda3016054ce5608e6df32155dd6d244b6678914
Bekithemba T. Matshazi <74741869+BT-Matshazi@users.noreply.github.com>
2024-09-02 10:55:12 +0200

    Update about.tsx

--- app/home/about/about.tsx
+++ app/home/about/about.tsx
@@ -6,7 +6,7 @@ export default function about() {
       <section className="flex items-center py-20 bg-stone-100 font-poppins">
         <div className="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
           <div className="flex flex-wrap ">
-            <div className="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+{/*             <div className="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
               <div className="relative">
                 <Image
                   src="https://images.pexels.com/photos/196659/pexels-photo-196659.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -16,7 +16,7 @@ export default function about() {
                   className="relative z-40 object-cover w-full h-96 lg:rounded-tr-[80px] lg:rounded-bl-[80px] rounded"
                 />
                 <div className="absolute z-10 hidden w-full h-full bg-accent rounded-bl-[80px] rounded -bottom-6 right-6 lg:block"></div>
-              </div>
+              </div> */}
             </div>
             <div className="w-full px-4 mb-10 lg:w-1/2 lg:mb-0 align-middle">
               <div className="relative">
